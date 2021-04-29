0ff3c948c9755fb3edd3562f88f8fe7651f2b4ef
Ivan Tanev <vankata.t@gmail.com>
2021-04-29 20:57:04 +0300

    Update example to build

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,10 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { assertEvent } from '../.';
 
 const App = () => {
-  return (
-    <div>
-      <Thing />
-    </div>
-  );
+  return <div>{assertEvent.toString()}</div>;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
