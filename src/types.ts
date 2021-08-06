a9a5aea2ff0bc89ad72372a2c6f928394a16bed4
Ivan Tanev <vankata.t@gmail.com>
2021-08-06 17:12:48 +0300

    Fix prettier error

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,6 @@
 export type ReducerWithoutAction<S> = (prevState: S) => S;
-export type ReducerStateWithoutAction<
-  R extends ReducerWithoutAction<any>
-> = R extends ReducerWithoutAction<infer S> ? S : never;
+export type ReducerStateWithoutAction<R extends ReducerWithoutAction<any>> =
+  R extends ReducerWithoutAction<infer S> ? S : never;
 
 export type Reducer<S, A> = (prevState: S, action: A) => S;
 export type ReducerState<R extends Reducer<any, any>> = R extends Reducer<infer S, any> ? S : never;
